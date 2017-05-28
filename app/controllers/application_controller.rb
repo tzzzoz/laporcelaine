b7b0af30b08a2922d2518ea7acc8e9bd2ca22c09
baicheng <tzzzoz@gmail.com>
2017-05-28 11:57:11 +0200

    mock slideshow_images and test slideshow effects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :slideshow_images
+
+  protected
+
+  def slideshow_images
+    (1..5).map { |i| "/upload/SONG_#{i.to_s.rjust(2, '0')}.jpg" }
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def slideshow_list(images)
+    content_tag(:ul, id: "slideshow") do
+      images.collect do |img_src|
+        concat content_tag(:li, nil) { image_tag(img_src, alt: "slideshow image") }
+      end
+    end
+  end
 end
